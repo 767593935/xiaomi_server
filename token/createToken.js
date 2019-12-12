50f0c74403d44263bd2a04004002f8a07c1f3133
Liu Yuxi <767593935@qq.com>
2019-12-12 10:01:04 +0800

    项目后台

---
+++ token/createToken.js
@@ -0,0 +1,14 @@
+// 安装jsonwebtoken
+// 引入jsonwebtoken
+var jwt = require('jsonwebtoken');
+// 导出
+module.exports=function(id){
+  // 根据id生成token
+  // 生成签名,设置有效期
+  // const token=jwt.sign({id},'secret',{expiresIn:'7 days'})
+  //const token=jwt.sign({id},'secret',{expiresIn:'20 s'})
+  const token=jwt.sign({id},'secret',{expiresIn:'1 days'})
+  //const token=jwt.sign({id},'secret',{expiresIn:'10 s'})
+  // 返回token
+  return token
+}
